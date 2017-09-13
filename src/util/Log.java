762c3a5d06b6c49aaf6f70b6e19b29031f29fc36
Cooy <Cooy_Zhong@163.com>
2017-09-13 10:58:14 +0800

    update Log util

--- src/util/Log.java
+++ src/util/Log.java
@@ -7,11 +7,17 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Reporter;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,6 +27,64 @@ import java.util.List;
  */
 
 public class Log {
+    private static Logger logger = LoggerFactory.getLogger(Log.class);
+
+    private static final ThreadLocal<DateFormat> DATE_FORMAT = new ThreadLocal<DateFormat>(){
+        @Override
+        protected DateFormat initialValue(){
+            return new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]");
+        }
+    };
+
+    public static void warn(String message, Object... args){
+        Reporter.log(getPrefix()+String.format(message, args)+"<br>");
+    }
+
+    public static void info(String message, Object... args){
+        String messages = getPrefix()+String.format(message, args);
+        logger.info(messages);
+        Reporter.log(messages+"<br>");
+    }
+
+    public static void info2(String message, Object... args){
+        String messages = getPrefix()+String.format(message, args);
+        logger.info(messages);
+        Reporter.log("<span style=\"color:green\">"+messages+"</span><br>");
+    }
+
+    public static void infoSuccess(String message, Object... args){
+        String messages = getPrefix()+String.format(message, args);
+        logger.info(messages);
+        Reporter.log("<span style=\"color:green\">"+messages+"</span><br>");
+    }
+
+    public static void infoFailed(String message, Object... args){
+        String messages = getPrefix()+String.format(message, args);
+        logger.info(messages);
+        Reporter.log("<span style=\"color:red\">"+messages+"</span><br>");
+    }
+
+    public static void infoSkipped(String message, Object... args){
+        String messages = getPrefix()+String.format(message, args);
+        logger.info(messages);
+        Reporter.log("<span style=\"color:blue\">"+messages+"</span><br>");
+    }
+
+    /**
+     * 获取堆栈轨迹的方法和类名，作为用例执行log的前缀。
+     * @return
+     */
+    private static String getPrefix(){
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        if (stackTraceElements == null || stackTraceElements.length < 4){
+            return "[BOGUS]";
+        }
+        String className = stackTraceElements[3].getClassName();
+        String methodName = stackTraceElements[3].getMethodName();
+        className = className.replaceAll("[a-z\\.]","");
+        String timestamp = DATE_FORMAT.get().format(new Date());
+        return String.format("%s [%s.%s]", timestamp, className, methodName);
+    }
 
     public boolean InitLog4j2(String time){
         boolean init=false;
